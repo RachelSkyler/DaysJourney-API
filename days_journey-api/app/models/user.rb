e965618fd140df8b19509f137094ddbbed63a5be
skylershin <munkyu.s.shin@gmail.com>
2014-04-27 11:36:23 +0900

    fix commit

--- days_journey-api/app/models/user.rb
+++ days_journey-api/app/models/user.rb
@@ -58,11 +58,11 @@ class User
   has_many :paths, validate: false, inverse_of: :user
   
   # Fields that are required in order to have a valid user
-  #validates :email, :encrypted_password, :user_name, :sign_in_count, presence: true
+  validates :email, :encrypted_password, :user_name, :sign_in_count, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticrememberableatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
